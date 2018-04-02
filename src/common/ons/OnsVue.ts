21787943bacfa46cff5bce6024aa7316157c2e0d
wingland <illusion1125@gmail.com>
2018-04-02 16:31:25 +0800

    Hello world example based on onesenui

---
+++ src/common/ons/OnsVue.ts
@@ -0,0 +1,5 @@
+
+import Vue from 'vue';
+export class OnsVue extends Vue {
+  protected $ons: any;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+
+import 'onsenui/css/onsen-css-components.css';
+import 'onsenui/css/onsenui.css';
 import Vue from 'vue';
+// @ts-ignore
+import VueOnsenUI from 'vue-onsenui';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import { router } from './routes';
 
 Vue.use(VueRouter);
+Vue.use(VueOnsenUI);
 
 const app = new Vue({
   el: '#app',
